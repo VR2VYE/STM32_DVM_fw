7ebe8c7e684b33781a7ecb8ea38efbbd812ef6a5
VR2VYE <35602032+VR2VYE@users.noreply.github.com>
2018-03-02 23:52:10 +0800

    Create stm32_dvm_fw.sh

---
+++ stm32_dvm_fw.sh
@@ -0,0 +1,17 @@
+# Configure latest version
+FW_VERSION="v1.3.3"
+
+git clone https://github.com/VR2VYE/STM32_DVM_fw.git
+
+cd bin
+
+# Download latest firmware for ZUMspot Libre Kit
+curl -OL https://github.com/VR2VYE/STM32_DVM_fw/releases/download/$FW_VERSION/mmdvm.hex
+
+cd
+
+sudo pistar-watchdog.service stop
+sudo systemctl stop mmdvmhost.timer
+sudo systemctl stop mmdvmhost.service
+
+program-STM32_DVM_PiHat
