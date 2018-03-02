49e302774116ca2aab8b4bb2ab60b41f3979462f
VR2VYE <35602032+VR2VYE@users.noreply.github.com>
2018-03-03 00:11:21 +0800

    Update stm32_dvm_fw.sh

--- stm32_dvm_fw.sh
+++ stm32_dvm_fw.sh
@@ -1,20 +1,23 @@
 # Configure latest version
 FW_VERSION="v1.3.3"
 
-mkdir /MMDVM/bin
+mkdir MMDVM
 
 cd MMDVM
 
 curl -OL https://github.com/VR2VYE/STM32_DVM_fw/master/Makefile.CMSIS
+
+mkdir bin
 
 cd bin
 
 # Download latest firmware for ZUMspot Libre Kit
 curl -OL https://github.com/VR2VYE/STM32_DVM_fw/releases/download/$FW_VERSION/mmdvm.hex
 
-cd
+cd ..
 
 sudo pistar-watchdog.service stop
 sudo systemctl stop mmdvmhost.timer
 sudo systemctl stop mmdvmhost.service
 
+program-STM32_DVM_PiHat
